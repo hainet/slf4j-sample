9492eeef96edbbd32b2655c8a22fe636e2fdf94a
hainet <clann.libertybell@gmail.com>
2017-08-12 19:25:24 +0900

    ロガー生成の記述を変更

--- src/main/java/com/hainet/slf4j/sample/Slf4jSampleApplication.java
+++ src/main/java/com/hainet/slf4j/sample/Slf4jSampleApplication.java
@@ -3,11 +3,13 @@ package com.hainet.slf4j.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
+
 public class Slf4jSampleApplication {
 
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Slf4jSampleApplication.class);
+    final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    public static void main(String[] args) {
         logger.info("Hello {}!", "hainet");
         logger.info("Hello Throwable!", new RuntimeException("This is RuntimeException!"));
         logger.info("{} / \\{}", "Replaced!");
